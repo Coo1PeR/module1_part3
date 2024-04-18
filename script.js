ccd785d63159be0ff0367d4f42e9768e9d39a47c
Coo1PeR <coo2per@gmail.com>
2024-04-18 18:02:57 +0300

    feat: add ability to login

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const btnLogin = document.querySelector('.btnLogin');
 
+let xhr = new XMLHttpRequest();
+xhr.open(
+  'POST',
+  'https://hjdjs55gol.execute-api.us-east-1.amazonaws.com/api/login'
+);
+
+btnLogin.addEventListener('click', e => {
+  e.preventDefault();
+
+  const user = {
+    email: document.getElementById('email').value,
+    password: document.getElementById('password').value,
+  };
+
+  console.log(user);
+  xhr.send(JSON.stringify(user));
+});
+
+xhr.onload = () => {
+  const res = JSON.parse(xhr.response);
+
+  if (res.token === 'token') {
+    localStorage.setItem('token', res.token);
+  } else {
+    alert(xhr.response);
+  }
+};
+
+//localStorage.clear()
+
+//po3FGas8;
