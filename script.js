56c7a74783aa902eff89b2d800a59e5f28f890af
Coo1PeR <coo2per@gmail.com>
2024-04-19 17:35:11 +0300

    feat: add rendering of gallary

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const TOKEN_KEY = 'token';
-const TOKEN_LIFE_TIME = 10 * 60 * 1000;
+const TOKEN_KEY = 'token'; // expected token
+const TOKEN_LIFE_TIME = 10 * 60 * 1000; // 10 minutes
+
 const btnLogin = document.querySelector('.btnLogin');
+const btnShowGallery = document.querySelector('.btnShowGallery');
+const btnPrev = document.querySelector('.previos');
+const btnNext = document.querySelector('.next');
 
 function clearLocalStorage() {
   localStorage.removeItem(TOKEN_KEY);
@@ -27,9 +31,6 @@ async function login(user) {
       localStorage.setItem(TOKEN_KEY, resultToken);
       setTimeout(clearLocalStorage, TOKEN_LIFE_TIME);
 
-      const token = {
-        Authorization: resultToken,
-      };
       alert('You have successfully logged in!');
     }
   } catch (error) {
@@ -58,4 +59,57 @@ btnLogin.addEventListener('click', e => {
 
   //console.log(JSON.stringify(user));
   login(user);
+});
+
+async function getGallery(i) {
+  try {
+    const responseGallery = await fetch(
+      `https://hjdjs55gol.execute-api.us-east-1.amazonaws.com/api/gallery?page=${i}`,
+      {
+        headers: {
+          Authorization: localStorage.getItem('token'),
+        },
+      }
+    );
+
+    const resultGallery = await responseGallery.json();
+    console.log(resultGallery);
+
+    const images = resultGallery.objects;
+
+    images.forEach(src => {
+      const img = document.createElement('img');
+      img.src = src;
+      img.width = '300';
+      img.id = 'img';
+
+      document.querySelector('#imgs').append(img);
+    });
+  } catch (error) {
+    alert(error);
+  }
+}
+
+btnShowGallery.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (localStorage.getItem('token')) {
+    getGallery(2);
+  }
+});
+
+function cleanGallery() {
+  document.querySelectorAll('#img').forEach(el => el.remove());
+}
+
+btnNext.addEventListener('click', e => {
+  e.preventDefault();
+
+  cleanGallery();
+});
+
+btnPrev.addEventListener('click', e => {
+  e.preventDefault();
+
+  cleanGallery();
 });
