c649b10e762289494b500dcaf20bcc54e5493683
Coo1PeR <coo2per@gmail.com>
2024-04-19 15:15:29 +0300

    refactor: rewrite logic on async/await

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
+const TOKEN_KEY = 'token';
+const TOKEN_LIFE_TIME = 10 * 60 * 1000;
 const btnLogin = document.querySelector('.btnLogin');
 
-let xhr = new XMLHttpRequest();
-xhr.open(
-  'POST',
-  'https://hjdjs55gol.execute-api.us-east-1.amazonaws.com/api/login'
-);
+function clearLocalStorage() {
+  localStorage.removeItem(TOKEN_KEY);
+  location.reload();
+}
+
+async function login(user) {
+  try {
+    const responseLogin = await fetch(
+      'https://hjdjs55gol.execute-api.us-east-1.amazonaws.com/api/login',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(user),
+      }
+    );
+
+    const resultLogin = await responseLogin.json();
+    const resultToken = resultLogin.token;
+
+    if (resultToken === TOKEN_KEY) {
+      localStorage.setItem(TOKEN_KEY, resultToken);
+      setTimeout(clearLocalStorage, TOKEN_LIFE_TIME);
+
+      const token = {
+        Authorization: resultToken,
+      };
+      alert('You have successfully logged in!');
+    }
+  } catch (error) {
+    alert(error);
+    //console.error('Error:', error);
+  }
+}
+
+//po3FGas8
 
 btnLogin.addEventListener('click', e => {
   e.preventDefault();
 
+  const passwordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,8}$/;
+  const mail = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  if (!passwordValid.test(password)) {
+    return alert('Password is not valid');
+  }
+
   const user = {
-    email: document.getElementById('email').value,
-    password: document.getElementById('password').value,
+    email: mail,
+    password: password,
   };
 
-  console.log(user);
-  xhr.send(JSON.stringify(user));
+  //console.log(JSON.stringify(user));
+  login(user);
 });
-
-xhr.onload = () => {
-  const res = JSON.parse(xhr.response);
-
-  if (res.token === 'token') {
-    localStorage.setItem('token', res.token);
-  } else {
-    alert(xhr.response);
-  }
-};
-
-//localStorage.clear()
-
-//po3FGas8;
