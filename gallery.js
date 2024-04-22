b77d6608bf9e4fa07cba9c8c3962ac378d706f3b
Coo1PeR <coo2per@gmail.com>
2024-04-23 00:20:58 +0300

    fix: added page navigation from the address bar

--- gallery.js
+++ gallery.js
@@ -1,9 +1,9 @@
 ///////////////////////////////////////
 // Gallery
-const btnShowGallery = document.querySelector('.btnShowGallery');
-const btnPrev = document.querySelector('.previos');
-const btnNext = document.querySelector('.next');
-const btnPages = document.querySelectorAll('#pages');
+const btnShowGallery = document.querySelector(".btnShowGallery");
+const btnPrev = document.querySelector(".previos");
+const btnNext = document.querySelector(".next");
+const btnPages = document.querySelectorAll("#pages");
 
 // Will be recorded in the getGallery
 let currentPage = 1;
@@ -17,7 +17,7 @@ async function getGallery(i) {
       `https://hjdjs55gol.execute-api.us-east-1.amazonaws.com/api/gallery?page=${i}`,
       {
         headers: {
-          Authorization: localStorage.getItem('token'),
+          Authorization: localStorage.getItem("token"),
         },
       }
     );
@@ -25,48 +25,45 @@ async function getGallery(i) {
     const resultGallery = await responseGallery.json();
     currentPage = resultGallery.page;
     maxPage = resultGallery.total;
-    localStorage.setItem('page', resultGallery.page);
+    localStorage.setItem("page", resultGallery.page);
 
-    currentUrl = new URL(`${window.location}gallery?page=${i}`);
-    console.log(currentUrl);
-
-    console.log(resultGallery);
+    updateURLParams();
 
     // Rendering images
     const images = resultGallery.objects;
 
-    images.forEach(src => {
-      const img = document.createElement('img');
+    images.forEach((src) => {
+      const img = document.createElement("img");
       img.src = src;
-      img.width = '300';
-      img.id = 'img';
+      img.width = "300";
+      img.id = "img";
 
-      document.querySelector('#imgs').append(img);
+      document.querySelector("#imgs").append(img);
     });
   } catch (error) {
-    alert(error);
+    alert(`Error:${error}`);
   }
 }
 
-btnShowGallery.addEventListener('click', element => {
+btnShowGallery.addEventListener("click", (element) => {
   element.preventDefault();
 
-  if (localStorage.getItem('token')) {
-    getGallery(localStorage.getItem('page') || currentPage);
+  if (localStorage.getItem("token")) {
+    getGallery(localStorage.getItem("page") || currentPage);
   }
 });
 
 function cleanGallery() {
-  document.querySelectorAll('#img').forEach(element => element.remove());
+  document.querySelectorAll("#img").forEach((element) => element.remove());
 }
 
-btnNext.addEventListener('click', element => {
+btnNext.addEventListener("click", (element) => {
   element.preventDefault();
 
   nextPage();
 });
 
-btnPrev.addEventListener('click', element => {
+btnPrev.addEventListener("click", (element) => {
   element.preventDefault();
 
   prevPage();
@@ -94,15 +91,38 @@ function prevPage() {
 
 // Open last page after reload
 function reloadPage() {
-  if (localStorage.getItem('token')) {
-    getGallery(localStorage.getItem('page'));
+  if (localStorage.getItem("token")) {
+    getGallery(localStorage.getItem("page"));
   }
 }
 
-btnPages.forEach(element => {
-  element.addEventListener('click', () => {
+btnPages.forEach((element) => {
+  element.addEventListener("click", () => {
     getGallery(element.innerHTML);
   });
 });
+
+// get value from URL
+function getPageParameter() {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  return urlParams.get("page");
+}
+
+// handle page load event
+window.onload = function () {
+  const pageNumber = getPageParameter();
+  if (pageNumber !== null) {
+    getGallery(pageNumber);
+  }
+};
+
+// generate new url
+function updateURLParams() {
+  const params = new URLSearchParams(window.location.search);
+  params.set("page", currentPage);
+  const newUrl = `${window.location.pathname}?${params.toString()}`;
+  window.history.pushState({}, "", newUrl);
+}
 
 export {};
